cfd835299456be8a1b2c3487e9e8c1f98abdc2b6
RobertJMitchell <rjrocks32@gmail.com>
2015-03-26 18:55:13 -0500

    standard asp.NET MVC app created
    standard asp.NET MVC app created, bootstrap included, created two new
    classes Album and Artist

---
+++ mvcMVA/Models/Artist.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mvcMVA.Models
+{
+    public class Artist
+    {
+        public int ArtistID { get; set; }
+    }
+}
